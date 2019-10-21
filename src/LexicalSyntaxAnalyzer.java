779857078cb290ffc5c885d37f591a4ef7567992
Farhan Shahbaz <49499937+Farhan12345a@users.noreply.github.com>
2019-10-20 22:24:36 -0400

    Update LexicalSyntaxAnalyzer.java

--- src/LexicalSyntaxAnalyzer.java
+++ src/LexicalSyntaxAnalyzer.java
@@ -16,7 +16,120 @@ public class LexicalSyntaxAnalyzer {
          */
         return false;
     }
+    
+    //Lexical Analyzer Methods:
+    
+     public boolean int_literal(){
+        /*
+            calls for digit repeatedly if needed
+         */
+        return false;
+    }
+    
+     public boolean procedure_keyword(){
+        /*
+            Checks to see if procedure keyword
+         */
+        return false;
+    }
+    
+     public boolean begin_keyword(){
+        /*
+           Checks to see for begin keyword
+         */
+        return false;
+    }
+    
+     public boolean end_keyword(){
+        /*
+            Checks to see for end keyword
+         */
+        return false;
+    }
+    
+     public boolean semicolon(){
+        /*
+            Checks to see if token is a semicolon
+         */
+        return false;
+    }
+    
+     public boolean assignment_op(){
+        /*
+            Checks to see if token is a assignment operator
+         */
+        return false;
+    }
+    
+     public boolean addiion_op(){
+        /*
+            Checks to see if token is a '+'
+         */
+        return false;
+    }
+    
+     public boolean subtraction_op(){
+        /*
+            checks to see if token is a '-'
+         */
+        return false;
+    }
+    
+     public boolean multiplication_op(){
+        /*
+            Checks to see if token is '*'
+         */
+        return false;
+    }
+    
+     public boolean division_op(){
+        /*
+            checks to see if token is '/'
+         */
+        return false;
+    }
+    
+     public boolean equality_op(){
+        /*
+            Checks to see if '==' is the token
+         */
+        return false;
+    }
+    
+     public boolean notequal_op(){
+        /*
+            Checks to see if '!=' is the token
+         */
+        return false;
+    }
+    
+     public boolean parenthesis(){
+        /*
+           Checks to see if '(' or ')' are used
+         */
+        return false;
+    }
+    
+     public boolean If(){
+       /*
+            checks for keyword if(
+            then calls for bool
+            checks for keywords ) then
+            calls for stmtList
+            checks for keyword else, if present calls for stmtList
+            checks for keyword endif
+        */
 
+        return false;
+    }
+    
+     public boolean end_of_input(){
+       /*
+            checks to see if file reaches end of input
+        */
+
+        return false;
+    }
     public boolean stmtList(){
         /*
             calls for stmt
@@ -29,19 +142,15 @@ public class LexicalSyntaxAnalyzer {
         //calls for assign statement or if statement
         return false;
     }
-
-    public boolean If(){
-       /*
-            checks for keyword if(
-            then calls for bool
-            checks for keywords ) then
-            calls for stmtList
-            checks for keyword else, if present calls for stmtList
-            checks for keyword endif
-        */
-
-        return false;
-    }
+    
+    /*
+       function lex():
+       invoked to retrive the next token from the input file. The input file s read character by characer.
+       Depending on character read characters are joined into lexemes. 
+       The lex function will return an integer representing one of the following oken classes or lexeme types
+    */
+    public 
+   
 
     public boolean assign(){
        /*
@@ -107,10 +216,5 @@ public class LexicalSyntaxAnalyzer {
         return false;
     }
 
-    public boolean Int(){
-        /*
-            calls for digit repeatedly if needed
-         */
-        return false;
-    }
+   
 }
